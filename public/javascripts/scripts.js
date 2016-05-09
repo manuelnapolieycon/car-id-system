04f486680744ff212ed8c4ce9923ad0ffe1366f8
manuelnapolieycon <manuelnapoli@eycon.com>
2016-05-09 09:17:46 -0300

    First commit

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,26 @@
+var socket = io.connect('http://localhost:3000', { 'forceNew': true });
+
+socket.on('messages', function(data) {  
+  document.getElementById('plate1').innerHTML = data;
+})
+
+function render (data) {  
+  var html = data.map(function(elem, index) {
+    return(`<div>
+              <strong>${elem.author}</strong>:
+              <em>${elem.text}</em>
+            </div>`);
+  }).join(" ");
+
+  document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e) {  
+  var message = {
+    author: document.getElementById('username').value,
+    text: document.getElementById('texto').value
+  };
+
+  socket.emit('new-message', message);
+  return false;
+}
